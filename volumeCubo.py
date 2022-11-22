1c85b8fd36dfb6cd0587957e093f530b30c25a60
dambleerley <dambleerley@gmail.com>
2022-11-22 20:38:26 -0300

    Acrescentando m³ a resposta da classe

--- volumeCubo.py
+++ volumeCubo.py
@@ -3,6 +3,6 @@ class volumeCubo:
         
        aresta = float(input("Entre com um valor para a aresta do cubo: "))
        a = aresta**3
-       print(f"O volume do cubo é {a} m3")
+       print(f"O volume do cubo é {a} m³")
        
 volume = volumeCubo()
